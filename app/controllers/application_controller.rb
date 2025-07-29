4d78f9a17f1a9debdd8d82be3b5ad0e931d0949f
Samiul <s.a.samiru64@gmail.com>
2025-07-30 05:38:09 +0600

    Add bootstrap cdn link and Navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  
+  before_action :set_locale
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    # To keep locale in URLs
+    def default_url_options
+      { locale: I18n.locale }
+    end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module ShopEasyBd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :bn]
+    config.i18n.default_locale = :en
+
   end
 end
